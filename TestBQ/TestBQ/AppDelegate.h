4cce4004ee646c930ede4ee56a9a209b7aa9f8e4
Michel Barbou Salvador <mibarbou@gmail.com>
2015-12-15 18:51:46 +0100

    Added empty application project. Added .gitignore file.

---
+++ TestBQ/TestBQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestBQ
+//
+//  Created by Michel Barbou Salvador on 15/12/15.
+//  Copyright © 2015 MBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
